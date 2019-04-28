0279a6175c8a21df67c3c002a62cab53ae78d94c
ricki <root@rickywfu-PC2.tencent.com>
2019-04-28 21:51:17 +0800

    commit logme

---
+++ src/mongo/logme/logme.h
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <ctime>
+#include <chrono>
+class Logme {
+    friend Logme& GetGlobalLogme();
+    public:
+        ~Logme() {
+            _ofs.close();
+        }
+        template <typename T>
+        Logme& operator<<(const T& x) {
+            _ofs << x;
+            return *this;
+        }
+        Logme& operator<<(std::ostream& (*manip)(std::ostream&)) {
+            _ofs << manip;
+            return *this;
+        }
+    private:
+        Logme() {
+            _ofs.open("logme.log", std::ofstream::app);
+        }
+        std::ofstream _ofs;
+};
+
+Logme &GetGlobalLogme() {
+    static Logme logme;
+    auto now = std::chrono::system_clock::now();
+    auto milli = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+    time_t t = std::chrono::system_clock::to_time_t(now);
+    char buf[100];
+    std::strftime(buf, sizeof(buf), "%F %T.", localtime(&t));
+    logme._ofs << buf;
+    logme._ofs << (milli % 1000) << "\t";
+    return logme;
+}
